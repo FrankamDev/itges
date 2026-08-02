96e415a648295c88c5ad1ef90a52a77c740a8b69
FrankamDev <fkamgang06@gmail.com>
2026-08-02 22:12:13 +0200

    s

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { visualizer } from "rollup-plugin-visualizer";
-export default defineConfig({
-  
 
+export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
@@ -14,5 +13,4 @@ export default defineConfig({
     }),
   ],
 
-  
 });
